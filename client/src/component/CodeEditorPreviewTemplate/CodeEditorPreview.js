f9e52a50abf66509afadad917671c61d19cbe285
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-10-24 16:39:51 +0900

    feat: add html-to-pdf  * pdfmake, pdfkit, htmltopdf,  * add button

--- client/src/component/CodeEditorPreviewTemplate/CodeEditorPreview.js
+++ client/src/component/CodeEditorPreviewTemplate/CodeEditorPreview.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './CodeEditorPreview.scss'
+// html - to - pdf 
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+import htmlToPdfmake from 'html-to-pdfmake';
 var marked = require('marked');
 marked.setOptions({
     headerPrefix: "header-"
@@ -7,15 +12,24 @@ marked.setOptions({
 function CodeEditorPreview ({mydata}) {
  
     var value = marked(mydata.body||'');
-   
+    function printDocument() {
+        const doc = new jsPDF()
+        const pdfTable = document.getElementById("result_preview")
+        var html = htmlToPdfmake(pdfTable.innerHTML)
+        const documentDefinition = {content:html}
+        pdfMake.fonts
+        pdfMake.vfs = pdfFonts.pdfMake.vfs;
+        pdfMake.createPdf(documentDefinition).open()
+    }
   
     return (
         <>
             
             <div className="preview" >
-                <div  className="tab-pane result_preview active" 
+                <div id="result_preview"  className="tab-pane result_preview active" 
                     dangerouslySetInnerHTML={{__html:value}} style={{maxWidth: '100%',
                         height: 'auto'}}></div>
+                <button className="right_button" onClick={printDocument}>PDF 변환</button>
             </div>
       
         </>
